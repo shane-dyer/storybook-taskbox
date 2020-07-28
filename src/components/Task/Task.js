cdeff09cdc952cda380453d41580d18cd276284c
Shane Dyer <shane.dyer01@gmail.com>
2020-07-28 16:13:59 +1200

    Add spaces

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -17,6 +17,7 @@ export default function Task({
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
+
       <div className="title">
         <input type="text" value={title} placeholder="Input title" readOnly />
       </div>
